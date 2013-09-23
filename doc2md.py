887762e7d9923399743effe792d3058e378272fd
Thomas Gläßle <t_glaessle@gmx.de>
2013-09-23 04:44:06 +0200

    Replace doctrim() by inspect.cleandoc()

--- doc2md.py
+++ doc2md.py
@@ -46,42 +46,11 @@ hardly forseeable, if I will decide to improve on it in the near future.
 """
 import re
 import sys
+import inspect
 
 __all__ = ['doctrim', 'doc2md']
 
-def doctrim(docstring):
-    """
-    Remove indentation from docstring.
-
-    This is taken from:
-    http://www.python.org/dev/peps/pep-0257/#handling-docstring-indentation
-
-    sys.maxint had to be replaced by sys.maxsize for python3.
-
-    """
-    if not docstring:
-        return ''
-    # Convert tabs to spaces (following the normal Python rules)
-    # and split into a list of lines:
-    lines = docstring.expandtabs().splitlines()
-    # Determine minimum indentation (first line doesn't count):
-    indent = sys.maxsize
-    for line in lines[1:]:
-        stripped = line.lstrip()
-        if stripped:
-            indent = min(indent, len(line) - len(stripped))
-    # Remove indentation (first line is special):
-    trimmed = [lines[0].strip()]
-    if indent < sys.maxsize:
-        for line in lines[1:]:
-            trimmed.append(line[indent:].rstrip())
-    # Strip off trailing and leading blank lines:
-    while trimmed and not trimmed[-1]:
-        trimmed.pop()
-    while trimmed and not trimmed[0]:
-        trimmed.pop(0)
-    # Return a single string:
-    return '\n'.join(trimmed)
+doctrim = inspect.cleandoc
 
 def unindent(lines):
     """
